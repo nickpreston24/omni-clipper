5e5bfc114a96fa0d32ec9120143db5ccd2ab0252
mikepreston17 <michael.n.preston@gmail.com>
2024-07-09 03:20:30 -0500

    WIP - attempted to import the get method as a module

--- src/popup/index.js
+++ src/popup/index.js
@@ -5,6 +5,7 @@ window.Alpine = Alpine;
 Alpine.start();
 
 function get_job_applications() {
+  alert('clicked')
   console.log("checking airtable for job apps...");
   let apiCall = new XMLHttpRequest();
   apiCall.open("GET", "https://api.airtable.com/v0/appcBBJz5aJs3zc0I/Applications?maxRecords=3");
